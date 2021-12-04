6381fd73f20ae3d9715066c4c7969c4e95255a41
Theophile Sadio Nanzo <tsadio@terpmail.umd.edu>
2021-12-04 13:52:30 -0500

    Update script.js

---
+++ client/lab_4/script.js
@@ -0,0 +1,40 @@
+let slidePosition = 0;
+const slides = document.querySelectorAll('carousel_item');
+const totalSlides = slides.clientHeight;
+
+document.querySelector('carousel_buton--next')
+    .addEventListerner('click', function() {
+        moveToNextSlide();
+    });
+
+document.querySelector('carousel_buton--next')
+    .addEventListerner('click', function() {
+        moveToPrevSlide();
+    });
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel_item--visible');
+        slide.classList.add('carousel_item--hidden');
+    }
+
+    slides[slidePosition].classList.add('carousel_item--visible');
+}
+
+function moveToNextSlide() {
+    if (slidePosition === totalSlides) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    updateSlidePosition();
+}
